7e722605c5a4a45f627fcb22e4fc58f6aa8c026e
Anton Hvornum <anton@hvornum.se>
2023-03-07 20:24:13 +0100

    Fixed pytest runner

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -2,18 +2,19 @@ import pytest
 import datetime
 import sys
 import importlib
+import os
 
 original_argv = sys.argv
 
 def test_cli_single():
-	sys.argv = original_argv + ['--mirror', 'https://mirror.pseudoform.org/']
+	sys.argv = original_argv + ['--mirror', 'https://mirror.pseudoform.org/', '--tier0', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
 
 	import mirrortest.tooling.cli
 
 	mirrortest.tooling.cli.run()
 
 def test_cli_workers():
-	sys.argv = original_argv + ['--mirror', '*', '--workers', '2']
+	sys.argv = original_argv + ['--mirror', '*', '--workers', '2', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
 
 	import mirrortest.tooling.cli
 
@@ -22,7 +23,7 @@ def test_cli_workers():
 	mirrortest.tooling.cli.run()
 
 def test_cli_single_URLError():
-	sys.argv = original_argv + ['--mirror', 'https://broken.lan/']
+	sys.argv = original_argv + ['--mirror', 'https://broken.lan/', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
 
 	import mirrortest.tooling.cli
 
@@ -31,7 +32,7 @@ def test_cli_single_URLError():
 	mirrortest.tooling.cli.run()
 
 def test_cli_single_HTTPError():
-	sys.argv = original_argv + ['--mirror', 'https://archlinux.org/404_url']
+	sys.argv = original_argv + ['--mirror', 'https://archlinux.org/404_url', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
 
 	import mirrortest.tooling.cli
 
