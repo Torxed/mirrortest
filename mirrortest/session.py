28b13228282a64317ff201022949fe344b9c3cbf
Anton Hvornum <anton@hvornum.se>
2023-03-07 20:41:31 +0100

    Fixed pytest coverage

--- mirrortest/session.py
+++ mirrortest/session.py
@@ -8,7 +8,7 @@ from .models import Configuration
 configuration = Configuration()
 
 # Update configuration with stored JSON values (if any)
-if (config := pathlib.Path('~/.config/mirrortester/config.json').expanduser()).exists():
+if (config := pathlib.Path('~/.config/mirrortester/config.json').expanduser()).exists():  # pragma: no cover
 	with config.open() as fh:
 		json_config = json.load(fh)
 
