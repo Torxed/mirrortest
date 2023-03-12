a2d5b062ee2f11106d9fe464b678609c05e5ab54
Anton Hvornum <anton@hvornum.se>
2023-03-12 21:37:09 +0100

    Added --parse, --stats and --verbose.

--- mirrortest/tooling/cli.py
+++ mirrortest/tooling/cli.py
@@ -2,8 +2,11 @@ import dataclasses
 import argparse
 import pathlib
 import json
+import csv
 import time
 import sys
+import glob
+import sqlite3
 import urllib.error
 import urllib.request
 import pydantic
@@ -42,7 +45,6 @@ class MirrorTesterThreading(threading.Thread):
 					self.time_delta_int = self.time_delta_str.total_seconds()
 				else:  # pragma: no cover
 					self.time_delta_int = -5
-				self.good_exit = True
 			else:  # pragma: no cover
 				self.time_delta_str = 'Could not find /lastupdate on mirror'
 				self.time_delta_int = -6
@@ -63,7 +65,8 @@ class MirrorTesterThreading(threading.Thread):
 			self.time_delta_str = str(error)
 			self.time_delta_int = -4
 
-		self.good_exit = False
+		if self.good_exit is None:  # pragma: no cover 
+			self.good_exit = False
 
 
 # Parse script arguments and use defaults from configuration where needed
@@ -77,7 +80,7 @@ main_options.add_argument(
 )
 main_options.add_argument(
 	"--mirror",
-	required=True,
+	required=False,
 	default=None,
 	type=str,
 	nargs="?",
@@ -109,6 +112,27 @@ main_options.add_argument(
 	nargs="?",
 	help="When --mirror is set to '*', how many paralell workers do you want?"
 )
+main_options.add_argument(
+	"--parse",
+	required=False,
+	default=False,
+	action="store_true",
+	help="Parse a CSV log from a previous result and create a result database"
+)
+main_options.add_argument(
+	"--stats",
+	required=False,
+	default=False,
+	action="store_true",
+	help="Print statistics from a parsed result database"
+)
+main_options.add_argument(
+	"--verbose",
+	required=False,
+	default=False,
+	action="store_true",
+	help="Increse output verbosity"
+)
 args, unknown = main_options.parse_known_args()
 configuration.email = args.mail
 
@@ -116,7 +140,58 @@ configuration.email = args.mail
 def run():
 	tier_0 = Tier0(url=args.tier0)
 
-	if args.mirror != '*':
+	if args.mirror == '*':
+		# Retrieve complete mirror list
+		response = urllib.request.urlopen("https://archlinux.org/mirrorlist/all/")
+		data = response.read()
+		workers :list[MirrorTesterThreading] = []
+
+		with open(f'output_{time.time()}.log', 'w') as log:
+			for server in data.split(b'\n'):
+
+				if b'#Server = ' not in server:
+					continue
+				elif len(server.strip()) == 0:  # pragma: no cover
+					continue
+
+				if server.startswith(b'#Server'):
+					_, url = server.split(b'=', 1)
+					url, _ = url.split(b'/$repo', 1)
+					url = url.strip().decode()
+
+					while len(workers) >= args.workers and not any([worker.good_exit is not None for worker in workers]):
+						time.sleep(0.025)
+						if "pytest" in sys.modules:
+							break
+
+					finished_worker = None
+					for index, worker in enumerate(workers):
+						if worker.good_exit is not None:
+							finished_worker = workers.pop(index)
+							break
+
+					# Spawn a new worker
+					workers.append(MirrorTesterThreading(functools.partial(MirrorTester, tier=2, url=url, tier_0=tier_0)))
+
+					# And process the old one
+					if finished_worker is None:
+						continue
+
+					if not finished_worker.good_exit:  # pragma: no cover
+						log.write(f"{time.time()},{finished_worker.tester.keywords['url']},{finished_worker.time_delta_int},\"{finished_worker.time_delta_str}\"\n")
+						log.flush()
+
+					if "pytest" in sys.modules:
+						break
+
+			while any([worker.good_exit is None for worker in workers]):
+				time.sleep(0.025)
+
+			for worker in workers:
+				if not worker.good_exit:
+					log.write(f"{time.time()},{worker.tester.keywords['url']},{worker.time_delta_int},\"{worker.time_delta_str}\"\n")
+					log.flush()
+	elif args.mirror:
 		_error = None
 		_error_code = -1
 		try:
@@ -154,57 +229,82 @@ def run():
 
 					//Arch Linux mirror admins""".replace('\t', '')
 				)
-	else:
-		# Retrieve complete mirror list
-		response = urllib.request.urlopen("https://archlinux.org/mirrorlist/all/")
-		data = response.read()
-		workers :list[MirrorTesterThreading] = []
+	elif args.parse:
+		con = sqlite3.connect("results.db")
+		cur = con.cursor()
 
-		with open(f'output_{time.time()}.log', 'w') as log:
-			for server in data.split(b'\n'):
+		cur.execute("""
+			CREATE TABLE IF NOT EXISTS results(
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				unix_time UNIXEPOCH,
+				url VARCHAR(255),
+				seconds INT,
+				message VARCHAR(255),
+				UNIQUE(unix_time, url)
+			)""")
 
-				if b'#Server = ' not in server:
-					continue
-				elif len(server.strip()) == 0:  # pragma: no cover
-					continue
+		for log_file in glob.glob('./*.log'):
+			with open(log_file, newline='') as csvfile:
+				spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+				for row in spamreader:
+					try:
+						unix_timestamp, url, seconds, message = row
+					except ValueError as err:
+						if str(err).startswith('too many values to unpack') or str(err).startswith('not enough values to unpack'):
+							continue
+						else:  # pragma: no cover
+							raise err
 
-				if server.startswith(b'#Server'):
-					_, url = server.split(b'=', 1)
-					url, _ = url.split(b'/$repo', 1)
-					url = url.strip().decode()
+					cur.execute("""
+						INSERT INTO results (
+							unix_time, url, seconds, message
+						) VALUES (
+							?, ?, ?, ?
+						) ON CONFLICT DO NOTHING
+					""", (unix_timestamp, url, seconds, message))
 
-					while len(workers) >= args.workers and not any([worker.good_exit is not None for worker in workers]):
-						time.sleep(0.025)
-						if "pytest" in sys.modules:
-							break
+		con.commit()
+		cur.close()
+		con.close()
+	elif args.stats:
+		con = sqlite3.connect("results.db")
+		con.row_factory = sqlite3.Row
+		cur = con.cursor()
 
-					finished_worker = None
-					for index, worker in enumerate(workers):
-						if worker.good_exit is not None:
-							finished_worker = workers.pop(index)
-							break
+		problematic_mirrors = {}
+		erroneous_mirrors = {}
+		rows = cur.execute("""SELECT * FROM results WHERE DATETIME(ROUND(unix_time), 'unixepoch') >= DATETIME('now', '-10 days') ORDER BY url;""")
+		for row in rows.fetchall():
+			if not row['url'] in problematic_mirrors:
+				problematic_mirrors[row['url']] = {
+					'hits' : 0,
+					'proof' : [],
+					'oldest' : None,
+					'newest' : None
+				}
 
-					# Spawn a new worker
-					workers.append(MirrorTesterThreading(functools.partial(MirrorTester, tier=2, url=url, tier_0=tier_0)))
+			problematic_mirrors[row['url']]['hits'] += 1
+			problematic_mirrors[row['url']]['proof'].append(dict(row))
 
-					# And process the old one
-					if finished_worker is None:
-						continue
+			if problematic_mirrors[row['url']]['oldest'] is None or row['unix_time'] < problematic_mirrors[row['url']]['oldest']:
+				problematic_mirrors[row['url']]['oldest'] = row['unix_time']
+			if problematic_mirrors[row['url']]['newest'] is None or row['unix_time'] > problematic_mirrors[row['url']]['newest']:
+				problematic_mirrors[row['url']]['newest'] = row['unix_time']
 
-					if not finished_worker.good_exit:
-						log.write(f"{finished_worker.tester.keywords['url']},{finished_worker.time_delta_int},\"{finished_worker.time_delta_str}\"\n")
-						log.flush()
+			if problematic_mirrors[row['url']]['hits'] >= 3:
+				erroneous_mirrors[row['url']] = True
 
-					if "pytest" in sys.modules:
-						break
+		cur.close()
+		con.close()
 
-			while any([worker.good_exit is None for worker in workers]):
-				time.sleep(0.025)
-
-			for worker in workers:
-				if not worker.good_exit:
-					log.write(f"{worker.tester.keywords['url']},{worker.time_delta_int},\"{worker.time_delta_str}\"\n")
-					log.flush()
+		for url in erroneous_mirrors:
+			newest = datetime.datetime.fromtimestamp(problematic_mirrors[url]['newest']).date()
+			oldest = datetime.datetime.fromtimestamp(problematic_mirrors[url]['oldest']).date()
+			print(f"Between {oldest} - {newest}: {problematic_mirrors[url]['hits']} errors on {url}")
+			if args.verbose:
+				for error in problematic_mirrors[url]['proof']:
+					print(f"\t{error['message']}")
+		
 
 	# Upon exiting, store the given configuration used
 	config = pathlib.Path('~/.config/mirrortester/config.json').expanduser()
--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -22,6 +22,15 @@ def test_cli_workers():
 
 	mirrortest.tooling.cli.run()
 
+def test_cli_workers_singular():
+	sys.argv = original_argv + ['--mirror', '*', '--workers', '1', '--tier0', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
+
+	import mirrortest.tooling.cli
+
+	importlib.reload(mirrortest.tooling.cli)
+
+	mirrortest.tooling.cli.run()
+
 def test_cli_single_URLError():
 	sys.argv = original_argv + ['--mirror', 'https://broken.lan/', '--tier0', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
 
@@ -33,6 +42,46 @@ def test_cli_single_URLError():
 
 def test_cli_single_HTTPError():
 	sys.argv = original_argv + ['--mirror', 'https://archlinux.org/404_url', '--tier0', f"https://torxed:{os.environ['PASSWORD']}@repos.archlinux.org/$repo/os/$arch"]
+
+	import mirrortest.tooling.cli
+
+	importlib.reload(mirrortest.tooling.cli)
+
+	mirrortest.tooling.cli.run()
+
+def test_cli_parse():
+	sys.argv = original_argv + ['--parse']
+
+	test_data = ''
+	test_data += '1678652796.0924394,http://mirror.reisenbauer.ee/archlinux,-2,"<urlopen error [Errno -5] No address associated with hostname>"\n'
+	test_data += '1678652796.2621465,http://archlinux.mirror.colo-serv.net,-2,"<urlopen error [Errno -2] Name or service not known>"\n'
+	test_data += '1678652796.3114855,http://mirror.easyname.at/archlinux,75474.0,"20:57:54"\n'
+	test_data += '1678652796.3470361,http://archlinux.mirror.kangaroot.net,904879.0,"10 days, 11:21:19"\n'
+	test_data += '1678652796.3646152,http://mirrors.netix.net/archlinux,13515.0,"3:45:15"\n'
+	test_data += '1678653128.7254856,http://mirror.lebedinets.ru/archlinux,-4,"1 validation error for MirrorTester\n'
+	test_data += '__root__\n'
+	test_data += '  invalid literal for int() with base 10: b\'<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n    <meta name="viewport" content="width=device-width, initial-scale (type=value_error)"\'\n'
+
+	with open('./output_1678652848.097852.log', 'w') as fh:
+		fh.write(test_data)
+
+	import mirrortest.tooling.cli
+
+	importlib.reload(mirrortest.tooling.cli)
+
+	mirrortest.tooling.cli.run()
+
+def test_cli_stats():
+	sys.argv = original_argv + ['--stats']
+
+	import mirrortest.tooling.cli
+
+	importlib.reload(mirrortest.tooling.cli)
+
+	mirrortest.tooling.cli.run()
+
+def test_cli_stats_verbose():
+	sys.argv = original_argv + ['--stats', '--verbose']
 
 	import mirrortest.tooling.cli
 
